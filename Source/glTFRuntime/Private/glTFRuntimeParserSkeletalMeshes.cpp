683762af0458db1dda540e39e692277c7930508f
Sietze Riemersma <43845930+KungFuDonkey@users.noreply.github.com>
2023-02-06 15:38:43 +0100

    removed AnimDataController from includes

--- Source/glTFRuntime/Private/glTFRuntimeParserSkeletalMeshes.cpp
+++ Source/glTFRuntime/Private/glTFRuntimeParserSkeletalMeshes.cpp
@@ -19,7 +19,11 @@
 #include "AssetRegistryModule.h"
 #endif
 #endif
+
+#if ENGINE_MAJOR_VERSION >= 5 && ENGINE_MINOR_VERSION < 2
 #include "AnimDataController.h"
+#endif
+
 #include "Engine/SkeletalMeshSocket.h"
 #include "glTFAnimBoneCompressionCodec.h"
 #include "glTFAnimCurveCompressionCodec.h"
@@ -27,6 +31,7 @@
 #include "Animation/MorphTarget.h"
 #include "Async/Async.h"
 #include "Animation/AnimCurveTypes.h"
+#include "Engine/SkinnedAssetCommon.h"
 #include "PhysicsEngine/PhysicsAsset.h"
 #include "PhysicsEngine/PhysicsConstraintTemplate.h"
 #if ENGINE_MAJOR_VERSION > 4 || ENGINE_MINOR_VERSION > 25
