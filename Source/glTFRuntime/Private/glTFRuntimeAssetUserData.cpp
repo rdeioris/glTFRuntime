2c72b3e2dcff794562a9138f0ee4f72a8b838007
rdeioris <roberto.deioris@gmail.com>
2024-07-01 10:01:10 +0200

    added UserData prelinary api

---
+++ Source/glTFRuntime/Private/glTFRuntimeAssetUserData.cpp
@@ -0,0 +1,5 @@
+// Copyright 2024, Roberto De Ioris.
+
+
+#include "glTFRuntimeAssetUserData.h"
+
---
+++ Source/glTFRuntime/Public/glTFRuntimeAssetUserData.h
@@ -0,0 +1,17 @@
+// Copyright 2024, Roberto De Ioris.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetUserData.h"
+#include "glTFRuntimeAssetUserData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLTFRUNTIME_API UglTFRuntimeAssetUserData : public UAssetUserData
+{
+	GENERATED_BODY()
+	
+};
